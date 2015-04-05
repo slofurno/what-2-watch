595db72c0a8580add4ccb0e93215cb9c276cff7f
slofuno <slofurno@gmail.com>
2015-04-04 22:14:33 -0400

    first commit

---
+++ app/src/main/java/com/github/slofurno/what_2_watch/Actor.java
@@ -0,0 +1,28 @@
+package com.github.slofurno.what_2_watch;
+
+import java.util.ArrayList;
+
+/**
+ * Created by slofurno on 4/4/2015.
+ */
+public class Actor
+{
+    public int ActorId;
+    public String First;
+    public String Last;
+    public ArrayList<String> movies;
+
+    public Actor(){
+
+    }
+
+    public Actor(int id, String first, String last){
+        ActorId=id;
+        First=first;
+        Last=last;
+    }
+
+    public String toString(){
+        return First + " " + Last;
+    }
+}
---
+++ app/src/main/java/com/github/slofurno/what_2_watch/Movie.java
@@ -0,0 +1,18 @@
+package com.github.slofurno.what_2_watch;
+
+/**
+ * Created by slofurno on 4/4/2015.
+ */
+public class Movie {
+
+    public int MovieId;
+    public String title;
+    public String year;
+    public float rating;
+    public int votes;
+
+    public Movie(){
+
+    }
+
+}
