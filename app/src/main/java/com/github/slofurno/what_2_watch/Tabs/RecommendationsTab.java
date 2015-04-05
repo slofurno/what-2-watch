00abc75fb0b2e8a1e6694965b414db63692734c2
Sean Swag Phillips <sphillip@sleipnir.cs.csubak.edu>
2015-04-05 14:39:26 -0700

    Adding XML tabs

---
+++ app/src/main/java/com/github/slofurno/what_2_watch/Tabs/RecommendationsTab.java
@@ -0,0 +1,16 @@
+package com.github.slofurno.what_2_watch.Tabs;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.slofurno.what_2_watch.R;
+
+public class RecommendationsTab extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recommendations_layout, container, false);
+    }
+}
