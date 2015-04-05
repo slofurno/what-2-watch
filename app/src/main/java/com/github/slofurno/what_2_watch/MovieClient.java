96de3b1f6463dd8f7649c32d9d3d2d70f7dabda7
slofuno <slofurno@gmail.com>
2015-04-04 22:29:06 -0400

    adding some kind of api client

---
+++ app/src/main/java/com/github/slofurno/what_2_watch/MovieClient.java
@@ -0,0 +1,32 @@
+package com.github.slofurno.what_2_watch;
+
+import com.loopj.android.http.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Created by slofurno on 4/4/2015.
+ */
+public class MovieClient {
+    private static final String API_BASE_URL = "http://gdf3.com:555/api/";
+    private AsyncHttpClient client;
+
+    public MovieClient() {
+        this.client = new AsyncHttpClient();
+    }
+
+    private String getApiUrl(String relativeUrl) {
+        return API_BASE_URL + relativeUrl;
+    }
+
+    // Method for accessing the search API
+    public void getMovies(final String query, JsonHttpResponseHandler handler) {
+        try {
+            String url = getApiUrl("movies/");
+            client.get(url + URLEncoder.encode(query, "utf-8"), handler);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
